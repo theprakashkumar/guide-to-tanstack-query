29c90fc9671e38a92c4944e53c5f271b829f9ac8
theprakashkumar <maiprakashkumar@gmail.com>
2025-08-30 17:22:13 +0530

    feat: Added React Router.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import { Route, Routes } from "react-router";
 import "./App.css";
 
 function App() {
-  return <>Application</>;
+  return (
+    <Routes>
+      <Route index element={<h1>Home</h1>} />
+      <Route path="/about" element={<h1>About</h1>} />
+    </Routes>
+  );
 }
 
 export default App;
