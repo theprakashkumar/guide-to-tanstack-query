ed45d923d4537d63113ca3e2e0330785cdbf6948
theprakashkumar <maiprakashkumar@gmail.com>
2025-09-01 17:15:51 +0530

    feat: Added UseQuery page.

--- src/pages/UseQueryPage.jsx
+++ src/pages/UseQueryPage.jsx
@@ -1,5 +1,32 @@
+import { useQuery } from "@tanstack/react-query";
+import Loading from "../components/Loading";
+import PostCardList from "../components/PostCardList";
+import Button from "../components/Button";
+
 const UseQueryPage = () => {
-  return <div>UseQueryPage</div>;
+  const getPosts = async () => {
+    const response = await fetch("https://jsonplaceholder.typicode.com/posts/");
+    return await response.json();
+  };
+
+  const { data, isPending, refetch, error, isRefetching } = useQuery({
+    queryKey: ["posts"],
+    queryFn: getPosts,
+  });
+
+  if (isPending || isRefetching) return <Loading />;
+  if (error) return <div>{error.message}</div>;
+
+  return (
+    <>
+      <PostCardList cards={data} />
+      <Button
+        text="Refetch"
+        onClick={() => refetch()}
+        disabled={isRefetching}
+      />
+    </>
+  );
 };
 
 export default UseQueryPage;
